33b490bae9a982520991b706c5aa5e4fcbf917b4
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-10-15 11:46:34 -0400

    photo loading on modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -15,16 +15,29 @@ const PhotoDetailsModal = (props) => {
 
   //note, because they are stored in an array, photo 1 is element 0
   //hense photoID 1 - 1 = 0 in order to access it
-  const photoID = props.modal - 1;
-  console.log(photoID);
-  
+  // const photoID = props.modal
+  // const modalPhoto = props.photos[props.modal]
+  // console.log("element from photo details:", photoID);
+  const photoID = props.modal
   //this is a object of objects
   const similarPhotos = props.photos[photoID].similar_photos;
   console.log(`similarphotos by id: ${photoID}`, similarPhotos);
   //convert similarPhotos to an array? pass as prop to photo list...
   //refactor photo list to accept a prop (currently directly accepting photos via import)
-  const photos = Object.values(similarPhotos);
-  console.log("similar photos as value of photos: ", photos)
+  const similarPhotosList = Object.values(similarPhotos);
+  //console.log("similar photos as value of photos: ", photos)
+
+
+ 
+  const modalPhoto = props.photos[props.modal]
+  // console.log("element from photo details:", photoID);
+  // console.log("modal Photo", modalPhoto)
+  const modalPhotoImgSource = modalPhoto.urls.regular
+//  console.log("modalPhotoImgSource", modalPhotoImgSource)
+
+
+
+
   return (
 
     <div className="photo-details-modal">
@@ -38,16 +51,20 @@ const PhotoDetailsModal = (props) => {
         />
       </button>
       <PhotoListItem
-        //id={props.modal}
-        imageSource={props.photos.imageSource}
+        //the photo item id is set by the modal state
+        //think of how to pass the SPECIFIC photo info to this component here
+        id={props.modal}
+        imageSource={modalPhotoImgSource}
         favouritedList={props.favouritedList}
         setFavouritedList={props.setFavouritedList}
+
+
       />
       {/* add the data here from the photo */}
       {/* <PhotoList
         //id={props.modal}
         setModal={props.setModal}
-        photos={photos}
+        photos={similarPhotosList}
       /> */}
     </div>
   );
